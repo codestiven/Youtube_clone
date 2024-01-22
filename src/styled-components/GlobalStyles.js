41a9295939f11ca463b8e0d6481ba5ceac52235b
Stiven <codestiven@gmail.com>
2024-01-22 12:45:13 -0400

    GlobalStyles

---
+++ src/styled-components/GlobalStyles.js
@@ -0,0 +1,16 @@
+// App.js (o tu archivo principal)
+
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyles = createGlobalStyle`
+  /* Estilos globales */
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+  /* Otros estilos */
+`;
+
+
+export default GlobalStyles;
